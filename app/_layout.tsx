bc793038551b92c8531cdc0e0509ea856eac8858
Aleksandar <aleksandar97kg@gmail.com>
2024-07-31 19:42:58 +0200

    add welcome screen and auth modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,25 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { ActionSheetProvider } from '@expo/react-native-action-sheet';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 
-export default function RootLayout() {
+const InitialLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{headerShown: false}} />
     </Stack>
   );
 }
+
+const RootLayout = () => {
+  return (
+    <ActionSheetProvider>
+      <GestureHandlerRootView style={{flex: 1}}>
+        <StatusBar style="light" />
+        <InitialLayout />
+      </GestureHandlerRootView>
+    </ActionSheetProvider>
+  )
+}
+
+export default RootLayout;
